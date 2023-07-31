0f73b2d6c93e17d9a94a2406693cc035bcef37c1
matiasgasparg <matiasgasparg@gmail.com>
2023-07-30 23:29:21 -0300

    Agregado el script para hacer llamadas al back, cambios en html y css

--- datos.js
+++ datos.js
@@ -1,33 +1,70 @@
+// datos.js
+//Creamos la clase usuario
 class Usuario {
     constructor(email, password) {
         this.email = email;
         this.password = password;
     }
+//Utilizamos async declarar una funcion asyncronica y que el condigo continue
+    async verificarCredenciales() {
+        try {
+            //Hacemos la llamada al backend y utilizamos wait para esperar la respuesta y que continue cuando haya respuesta
+            const respuesta = await fetch("http://127.0.0.1:5000/users/login", {
+                //Declaramos el metodo de llamada, el encabezado , y convertimos los datos en lineas JSON, con stringfy
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: this.email,
+                    password: this.password
+                })
+            });
 
-    verificarCredenciales() {
-        // Verifica las credenciales del usuario (esto es solo un ejemplo)
-        return this.email === "matiasgasparg@gmail.com" && this.password === "contraseña";
+            if (!respuesta.ok) {
+                throw new Error("Error al realizar la solicitud.");
+            }
+
+            const data = await respuesta.json();
+
+            // Verifica si el inicio de sesión fue exitoso
+            if (data.message === "Login successful") {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
     }
+}
+
+// Función para manejar el envío del formulario
+function handleLoginFormSubmit(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const usuario = new Usuario(email, password);
+
+    usuario.verificarCredenciales()
+        .then((isAuthenticated) => {
+            if (isAuthenticated) {
+                alert("Inicio de sesión exitoso");
+            } else {
+                alert("Correo electrónico o contraseña incorrectos. Inténtalo de nuevo.");
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            alert("Ocurrió un error al realizar la autenticación. Inténtalo de nuevo más tarde.");
+        });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("login-form");
-
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-
-        // Obtiene los valores del formulario
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-
-        // Crea una instancia del usuario con los valores ingresados
-        const usuario = new Usuario(email, password);
-
-        // Verifica las credenciales utilizando el método de la clase Usuario
-        if (usuario.verificarCredenciales()) {
-            alert("Inicio de sesión exitoso");
-        } else {
-            alert("Correo electrónico o contraseña incorrectos. Inténtalo de nuevo.");
-        }
-    });
+    const submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", handleLoginFormSubmit);
 });
